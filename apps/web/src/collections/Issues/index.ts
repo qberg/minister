e9ae1131ed18b5782b3d43cf6e47a9547187da4d
qberg <qbergnoe@gmail.com>
2025-12-27 19:39:59 +0530

    feat(payload): added issues collection

---
+++ apps/web/src/collections/Issues/index.ts
@@ -0,0 +1,46 @@
+import type { CollectionConfig } from "payload";
+import {
+  ContentManagerAccess,
+  EveryoneAccess,
+} from "@/access/collection-level-access";
+import { createSlugField } from "@/Fields/slug";
+
+export const Issues: CollectionConfig<"issues"> = {
+  slug: "issues",
+  labels: {
+    singular: "Issue/Work Type",
+    plural: "Issues/Work Types",
+  },
+  access: {
+    create: ContentManagerAccess,
+    read: EveryoneAccess,
+    update: ContentManagerAccess,
+    delete: ContentManagerAccess,
+  },
+  admin: {
+    group: "Real Results of Alandur",
+    defaultColumns: ["name", "slug"],
+    useAsTitle: "name",
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      localized: true,
+      required: true,
+      label: "Name of the issue adressed",
+      admin: {
+        placeholder: "Road, Water,...",
+      },
+    },
+
+    ...createSlugField("name"),
+
+    {
+      name: "icon",
+      type: "upload",
+      relationTo: "media",
+      label: "Image",
+    },
+  ],
+};
